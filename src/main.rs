3bc2119d28d7f6ca613251385386be2ef3fd7285
hesamzkr <52599188+hesamzkr@users.noreply.github.com>
2021-11-29 14:15:51 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![allow(warnings)]
 use std::env;
-mod modules;
+mod solutions;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if let Some(q_num) = args.get(1) {
         match q_num.parse::<u32>() {
-            Ok(1) => modules::q1::Solution::run(),
+            Ok(1) => solutions::q1::Solution::run(),
             Err(i) => println!("Error: {}", i),
             _ => println!("Invalid question number"),
         }
