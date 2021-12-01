f01d09b7c6459c3caf5b6321e0bb367cbb5fedcc
Hesamaldin Zakeri <52599188+hesamzkr@users.noreply.github.com>
2021-12-01 09:53:17 +0000

    Q1 done

--- src/solutions/q1.rs
+++ src/solutions/q1.rs
@@ -8,16 +8,40 @@ impl Solution {
     pub fn run() {
         let q = Solution { number: 1 };
         println!("Question {}", q.number);
-        let input = fs::read_to_string("./inputs/q1.txt").expect("Something went wrong");
-        println!("Part A: {}", q.part_one());
-        println!("Part B: {}", q.part_two());
+        let input_str = fs::read_to_string("./inputs/q1.txt").expect("Something went wrong");
+        
+        let input: Vec<i32> = input_str.split("\n").map(|s| s.parse().expect("Can't parse to i32")).collect();
+
+        println!("Part A: {}", q.part_one(&input));
+        println!("Part B: {}", q.part_two(&input));
     }
 
-    fn part_one(&self) -> &str {
-        return "Answer to Part A";
+    fn part_one(&self, input: &Vec<i32>) -> i32 {
+        let mut previous = input[0];
+        let mut increased = 0;
+        for i in 1..input.len() {
+            if input[i] > previous {
+                increased += 1;
+            }
+            previous = input[i];
+        }
+
+        return increased;
     }
 
-    fn part_two(&self) -> &str {
-        return "Answer to Part B";
+    fn part_two(&self, input: &Vec<i32>) -> i32 {
+        let mut previous = input[0] + input[1] + input[2];
+        let mut increased = 0;
+        for i in 1..input.len() - 2 {
+            let new = input[i] + input[i + 1] + input[i + 2];
+            if new > previous {
+                increased += 1;
+            }
+
+            previous = new;
+
+        }
+
+        return increased;
     }
 }
